bbbe1df9cb76580d96b3f0b87696d271994c3010
Gretacm <getulee@gmail.com>
2025-10-07 17:23:41 +0300

    initial Cypress E2E setup

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    // baseUrl: 'https://www.inspirationanimation.com/',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
